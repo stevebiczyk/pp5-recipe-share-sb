17a02e485753bf25ea6e4aed0daa2cff4c99338a
stevebiczyk <stevebiczyk@gmail.com>
2023-07-12 14:48:38 +0000

    Created CurrentUserContext custom hook

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -14,10 +14,10 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/RegisterLogInForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import { SetCurrentUserContext } from "../../App";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function LogInForm() {
-  const setCurrentUser = useContext(SetCurrentUserContext);
+  const setCurrentUser = useSetCurrentUser();
   const [LogInData, setLogInData] = useState({
     username: "",
     password: "",
@@ -85,7 +85,7 @@ function LogInForm() {
             ))}
 
             <Button className={`${btnStyles.Button}`} type="submit">
-              Register
+              Log In
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
